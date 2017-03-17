0b44234780f3153f57cee8a4f40927331cb2f4ea
HeyItsFelipe <heyitsfelipe@gmail.com>
2017-03-17 00:15:54 -0700

    Cleaned up styles.css, renamed css classes, and removed unused css classes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     // Populate projects to portfolio page
     for(var i = 0; i < projects.length; i++){
         $('.project-row-js').append('<div class="col-xs-12 col-sm-6 col-md-4 project-' + i + '-js"></div>');
-        $('.project-' + i + '-js').append('<figure class="thumbnail project-thumbnail-height project-thumbnail-padding"><a href=' + projects[i].link + ' target="_blank"><img class="img-responsive project-img-center" src=' + projects[i].imgSrc + ' alt="Project 1 image"></a><figcaption class="caption"><h2 class="text-center text-uppercase project-title-margin project-title-style">' + projects[i].title + '</h2><p class="text-justify project-text-margin project-text-style">These are my notes from Udacity Intro to Programming Nanodegree program and my first website attempt.</p></figcaption></figure>');
+        $('.project-' + i + '-js').append('<figure class="thumbnail project-thumbnail-css"><a href=' + projects[i].link + ' target="_blank"><img class="img-responsive" src=' + projects[i].imgSrc + ' alt="Project 1 image"></a><figcaption class="caption"><h2 class="text-center text-uppercase">' + projects[i].title + '</h2><p class="text-justify">These are my notes from Udacity Intro to Programming Nanodegree program and my first website attempt.</p></figcaption></figure>');
         
     }
     
