310ea6e1d3f246d74b56648d3609897d6dacfc31
HeyItsFelipe <heyitsfelipe@gmail.com>
2017-03-22 22:34:42 -0700

    added techTags array to projects and modified css and media queries

--- script.js
+++ script.js
@@ -2,26 +2,28 @@
 $(document).ready(function(){
     
     // Project Constructor
-    function Project(title, link, imgSrc){
+    function Project(title, link, imgSrc, techTags){
     
         this.title = title;
         this.link = link;
         this.imgSrc = imgSrc;
+        this.techTags = techTags;
     
     }
     
     // List of projects
     var projects = [
-        new Project("Gandhi Tribute Page", "https://heyitsfelipe.github.io/GandhiTributePage/", "images/GandhiTribute_800x400.jpg"),
-        new Project("Neighborhood Map", "https://heyitsfelipe.github.io/Neighborhood_Map/", "images/NeighborhoodMap_400x200.jpg"),
-        new Project("IPND Notes", "https://first-project-1029.appspot.com/", "images/IPNDNotes_400x200.jpg")
+        new Project("Gandhi Tribute Page", "https://heyitsfelipe.github.io/GandhiTributePage/", "images/GandhiTribute_800x400.jpg", ["Dojo", "Bootstrap", "CSS", "HTML"]),
+        new Project("Neighborhood Map", "https://heyitsfelipe.github.io/Neighborhood_Map/", "images/NeighborhoodMap_400x200.jpg", ["Google Maps API", "Wikipedia API", "Knockout", "AJAX", "jQuery", "JavaScript", "Bootstrap", "CSS", "HTML"]),
+        new Project("IPND Notes", "https://first-project-1029.appspot.com/", "images/IPNDNotes_400x200.jpg", ["Google App Engine", "Python", "JavaScript", "CSS", "HTML"])
     
     ];
     
     // Populate projects to portfolio page
     for(var i = 0; i < projects.length; i++){
+        var projectTagsString = projects[i].techTags.join(', ');
         $('.project-row-js').append('<div class="col-xs-12 col-sm-6 col-md-4 project-' + i + '-js"></div>');
-        $('.project-' + i + '-js').append('<figure class="thumbnail project-thumbnail-css"><a href="' + projects[i].link + '" target="_blank"><img class="img-responsive" src="' + projects[i].imgSrc + '" alt="' + projects[i].title + ' project image"></a><figcaption class="caption"><h2 class="text-center text-uppercase">' + projects[i].title + '</h2><p class="text-justify"></p></figcaption></figure>');
+        $('.project-' + i + '-js').append('<figure class="thumbnail project-thumbnail-css"><a href="' + projects[i].link + '" target="_blank"><img class="img-responsive" src="' + projects[i].imgSrc + '" alt="' + projects[i].title + ' project image"></a><figcaption class="caption"><h2 class="text-center text-uppercase">' + projects[i].title + '</h2><p><em class="text-justify project-tags-css">' + projectTagsString + '</em></p></figcaption></figure>');
         
     }
     
